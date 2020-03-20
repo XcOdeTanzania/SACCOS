26073923ada3e90d193797a7bf9af8888655a459
dela728 <sumace01@gmail.com>
2020-03-20 13:50:27 +0300

    meetings updated

--- resources/views/pages/meetings/newMeetingInformation.blade.php
+++ resources/views/pages/meetings/newMeetingInformation.blade.php
@@ -16,23 +16,19 @@
                     </div>
                 </div>
                 <br></br>
-        <div class ="form-group row">
-            <label for="user" class="col-form-label col-md-3">Date:</label>
-                <div>
-                    <div class="input-group date AngleDate col-" >
-                        <input id="from" class="form-control" type="text"/>
-                        <span class="input-group-append input-group-addon">
-                        <span class="input-group-text fas fa-calendar-alt"></span></span>
+                <div class="form-group row mb-2"><label class="col-md-2 col-form-label mb-2">Date:</label>
+                    <div class="col-xl-6 col-10">
+                        <div class="input-group date" id="datetimepicker1"><input class="form-control" type="date"><span
+                                class="input-group-append input-group-addon"></span></div>
                     </div>
                 </div>
-        </div>
-        <br></br>
-        <div class="form-group row mb-2"><label class="col-md-2 col-form-label mb-2">Description:</label>
-            <div class="col-xl-6 col-10">
-                    <div> <textarea id="description" rows="4" cols="68"></textarea></div>
+                <br><br>
+                <div class="form-group row mb-2"><label class="col-md-2 col-form-label mb-2">Description:</label>
+                    <div class="col-xl-6 col-10">
+                        <div> <textarea id="description" rows="4" cols="68"></textarea></div>
                     </div>
-            </div>
-            <br><br>
+                </div>
+                <br><br>
                 <div> <a href="#"> <button class="btn btn-primary" type="submit">Add</button></a>
                       <a href="#"> <button class="btn btn-primary" type="submit">Delete</button></a>
                 </div>
