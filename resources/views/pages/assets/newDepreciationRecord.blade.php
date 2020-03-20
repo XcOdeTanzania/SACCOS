1d5d17527a935cc40e8c86026423d06d92a6d3ad
dela728 <sumace01@gmail.com>
2020-03-20 10:43:18 +0300

    asset pages updated

--- resources/views/pages/assets/newDepreciationRecord.blade.php
+++ resources/views/pages/assets/newDepreciationRecord.blade.php
@@ -9,32 +9,33 @@
     <div class =card card-default >
     <div class="card-body">
         <form class="form-horizontal" method="get" action="/">
-                <div class="form-group row mb-2"><label class="col-md-2 col-form-label mb-2">Description</label>
-                    <div class="col-xl-6 col-10">
-                        <div> <textarea id="description" rows="4" cols="68"></textarea></div>
-                    </div>
-                </div>
-                <br><br>
-                <div class="form-group row mb-2"><label class="col-md-2 col-form-label mb-2">Amount</label>
+                <div class="form-group row mb-2"><label class="col-md-2 col-form-label mb-2">Amount:</label>
                     <div class="col-xl-6 col-10">
                         <div class="input-group date" id="datetimepicker1"><input class="form-control" type="text"><span
                                 class="input-group-append input-group-addon"></span></div>
                     </div>
                 </div>
                 <br><br>
-                <div class="form-group row"><label class="col-md-2 col-form-label" for="input-id-1">Asset Category</label>
+                <div class="form-group row"><label class="col-md-2 col-form-label" for="input-id-1">Asset Category:</label>
                     <div class="col-md-6"><select name="accounttag" class="form-control" id="accountag">
                             <option>Select</option>
                             <option>Cars</option>
                             <option>Buildings</option>
                         </select></div>
                 </div><br><br>
-                <div class="form-group row mb-2"><label class="col-md-2 col-form-label mb-2">Date</label>
+                <div class="form-group row mb-2"><label class="col-md-2 col-form-label mb-2">Date:</label>
                     <div class="col-xl-6 col-10">
                         <div class="input-group date" id="datetimepicker1"><input class="form-control" type="date"><span
                                 class="input-group-append input-group-addon"></span></div>
                     </div>
                 </div>
+                <br></br>
+                <div class="form-group row mt-2"><label class="col-md-2 col-form-label">Description:</label>
+                    <div class="col-xl-6 col-10">
+                        <div> <textarea id="description" rows="4" cols="68"></textarea></div>
+                    </div>
+                </div>
+                <br><br>
                 <div> <a href="#"> <button class="btn btn-primary" type="submit">Add</button></a>
                     <a href="#"> <button class="btn btn-primary" type="submit">Delete</button></a>
                 </div>
